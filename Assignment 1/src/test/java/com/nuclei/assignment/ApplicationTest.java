30d486cc17e320d9cec93889587e4196bfa44a48
AjinkyaTaranekar <ajinkyataranekar@gmail.com>
2022-01-10 13:53:53 +0530

    - checkstyle.xml updated - checkstyle toolVersion upgrade - refactor based on recent checks

--- Assignment 1/src/test/java/com/nuclei/assignment/ApplicationTest.java
+++ Assignment 1/src/test/java/com/nuclei/assignment/ApplicationTest.java
@@ -4,14 +4,14 @@ import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import com.nuclei.assignment.constants.ExceptionsConstants;
 import com.nuclei.assignment.exception.CustomException;
-import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
-
 import java.io.ByteArrayInputStream;
 import java.io.ByteArrayOutputStream;
 import java.io.InputStream;
 import java.io.PrintStream;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
 
 
 /**
